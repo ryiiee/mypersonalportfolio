9906901e3727620a6b67417988875e7097cf629f
ryiiee <remoriezel@gmail.com>
2024-05-02 18:29:15 +0800

    add all link, and pictures

--- mypersonalportfolio/settings.py
+++ mypersonalportfolio/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    'C:/Users/Allyza/Desktop/mypersonalportfolio/personalportfolio/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
